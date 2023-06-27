24627866aa275a9c002ab874da9f66dc3bf70c23
vignesharau <vignesharau@gmail.com>
2023-06-27 13:30:54 +0530

    changes in 05

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,28 @@
+import { useState, useEffect } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+  return (
+    <div>
+      <h2>cleanup function</h2>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        Toggle me
+      </button>
+      {toggle && <RandomComponent />}
+    </div>
+  );
+};
+const RandomComponent = () => {
+  useEffect(() => {
+    console.log("hmm, this is interesting");
+    const intID = setInterval(() => {
+      console.log("hello from interval");
+    }, 1000);
+    return () => {
+      clearInterval(intID);
+    };
+  }, []);
+  return <h1>hello there</h1>;
 };
 
 export default CleanupFunction;
